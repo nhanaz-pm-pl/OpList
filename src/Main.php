f7d885389a0405c1e00dcf1720f48efaaa5b1279
HieuCoder <nguyenhieucoder47@gmail.com>
2022-02-03 21:10:14 +0700

    Create Main.php

--- src/Main.php
+++ src/Main.php
@@ -25,13 +25,13 @@ class Main extends PluginBase implements Listener {
 				if ($this->getServer()->getPlayerByPrefix(strval(($opName)))) {
 					$opOnline++;
 				}
+				$sender->sendMessage(
+				TF::GOLD . "List of operators " . TF::BLUE . "(Online: " . (($opOnline == 0) ? TF::RED . $opOnline :  TF::GREEN . $opOnline) . TF::BLUE . ")\n".
+				TF::YELLOW . "» " . TF::GREEN . $opNumbers . ". " . TF::BLUE . $opName . ($this->getServer()->getPlayerByPrefix(strval(($opName))) ? TF::GREEN . " (Online)" : TF::RED . " (Offline)"));
 			}
 			if (empty($opNumbers)) {
 				$sender->sendMessage(TF::YELLOW . "» " . TF::RED . "No operator");
 			}
-			$sender->sendMessage(
-				TF::GOLD . "List of operators " . TF::BLUE . "(Online: " . (($opOnline == 0) ? TF::RED . $opOnline :  TF::GREEN . $opOnline) . TF::BLUE . ")\n".
-				TF::YELLOW . "» " . TF::GREEN . $opNumbers . ". " . TF::BLUE . $opName . ($this->getServer()->getPlayerByPrefix(strval(($opName))) ? TF::GREEN . " (Online)" : TF::RED . " (Offline)"));
 		}
 		return true;
 	}
